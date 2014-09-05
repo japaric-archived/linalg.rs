0464d99b4caf30ee3dc423edcb9f8d6f69493419
Jorge Aparicio <japaric@linux.com>
2014-09-05 12:14:03 -0500

    Remove extra newline when printing matrices

--- src/show.rs
+++ src/show.rs
@@ -8,20 +8,27 @@ use {Mat, MutView, Trans, View};
 macro_rules! fmt {
     () => {
         fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let mut is_first_row = true;
             for row in self.rows() {
+                if is_first_row {
+                    is_first_row = false;
+                } else {
+                    try!(writeln!(f, ""));
+                }
+
                 try!(write!(f, "["));
 
-                let mut is_first = true;
+                let mut is_first_col = true;
                 for e in row.iter() {
-                    if is_first {
-                        is_first = false;
+                    if is_first_col {
+                        is_first_col = false;
                     } else {
                         try!(write!(f, ", "));
                     }
                     try!(write!(f, "{}", e));
                 }
 
-                try!(writeln!(f, "]"))
+                try!(write!(f, "]"))
             }
 
             Ok(())
