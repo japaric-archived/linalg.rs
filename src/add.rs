25d95dd243edea0d970b733644e419595c18bd51
Jorge Aparicio <japaricious@gmail.com>
2015-02-17 18:14:16 -0500

    feat: restore `impl`s like `1. + mat` now that orphan check is more flexible

--- src/add.rs
+++ src/add.rs
@@ -1,3 +1,4 @@
+use complex::Complex;
 use onezero::One;
 use std::ops::Add;
 
@@ -26,6 +27,18 @@ macro_rules! add0 {
     }
 }
 
+macro_rules! add0_ {
+    ($lhs:ty, $rhs:ty) => {
+        impl Add<$rhs> for $lhs {
+            type Output = $rhs;
+
+            fn add(self, rhs: $rhs) -> $rhs {
+                rhs + self
+            }
+        }
+    };
+}
+
 macro_rules! add1 {
     ($lhs:ty, $rhs:ty) => {
         impl<'a, T> Add<$rhs> for $lhs where T: Axpy + One {
@@ -45,6 +58,18 @@ macro_rules! add1 {
             }
         }
     }
+}
+
+macro_rules! add1_ {
+    ($lhs:ty, $rhs:ty) => {
+        impl<'a> Add<$rhs> for $lhs {
+            type Output = $rhs;
+
+            fn add(self, rhs: $rhs) -> $rhs {
+                rhs + self
+            }
+        }
+    };
 }
 
 macro_rules! add1c {
@@ -132,26 +157,45 @@ macro_rules! add3 {
 }
 
 // col
-/* FIXME(rust-lang/rust#20749)
-add0!(ColVec<T>, T);
-*/
 add1!(ColVec<T>, Col<'a, T>);
 add1!(ColVec<T>, &'a ColVec<T>);
 add2!(ColVec<T>, &'a MutCol<'b, T>);
 add1!(ColVec<T>, Scaled<T, Col<'a, T>>);
 
-/* FIXME(rust-lang/rust#20749)
-add1!(MutCol<'a, T>, T);
-*/
+impl<T> Add<T> for ColVec<T> where T: Axpy + One {
+    type Output = ColVec<T>;
+
+    fn add(mut self, rhs: T) -> ColVec<T> {
+        self.add_assign(rhs);
+        self
+    }
+}
+
+add0_!(f32, ColVec<f32>);
+add0_!(f64, ColVec<f64>);
+add0_!(Complex<f32>, ColVec<Complex<f32>>);
+add0_!(Complex<f64>, ColVec<Complex<f64>>);
+
 add2!(MutCol<'a, T>, Col<'a, T>);
 add2!(MutCol<'a, T>, &'a ColVec<T>);
 add3!(MutCol<'a, T>, &'a MutCol<'b, T>);
 add2!(MutCol<'a, T>, Scaled<T, Col<'a, T>>);
 
+impl<'a, T> Add<T> for MutCol<'a, T> where T: Axpy + One {
+    type Output = MutCol<'a, T>;
+
+    fn add(mut self, rhs: T) -> MutCol<'a, T> {
+        self.add_assign(rhs);
+        self
+    }
+}
+
+add1_!(f32, MutCol<'a, f32>);
+add1_!(f64, MutCol<'a, f64>);
+add1_!(Complex<f32>, MutCol<'a, Complex<f32>>);
+add1_!(Complex<f64>, MutCol<'a, Complex<f64>>);
+
 // mat
-/* FIXME(rust-lang/rust#20749)
-add0!(Mat<T>, T);
-*/
 add1!(Mat<T>, &'a Mat<T>);
 add2!(Mat<T>, &'a MutView<'b, T>);
 add1c!(Mat<T>, Scaled<T, View<'a, T>>);
@@ -160,9 +204,20 @@ add2!(Mat<T>, &'a Trans<MutView<'b, T>>);
 add1!(Mat<T>, Trans<View<'a, T>>);
 add1!(Mat<T>, View<'a, T>);
 
-/* FIXME(rust-lang/rust#20749)
-add1c!(MutView<'a, T>, T);
-*/
+impl<T> Add<T> for Mat<T> where T: Axpy + One {
+    type Output = Mat<T>;
+
+    fn add(mut self, rhs: T) -> Mat<T> {
+        self.add_assign(rhs);
+        self
+    }
+}
+
+add0_!(f32, Mat<f32>);
+add0_!(f64, Mat<f64>);
+add0_!(Complex<f32>, Mat<Complex<f32>>);
+add0_!(Complex<f64>, Mat<Complex<f64>>);
+
 add2!(MutView<'a, T>, &'b Mat<T>);
 add3!(MutView<'a, T>, &'b MutView<'c, T>);
 add2c!(MutView<'a, T>, Scaled<T, View<'b, T>>);
@@ -171,9 +226,20 @@ add3!(MutView<'a, T>, &'b Trans<MutView<'b, T>>);
 add2!(MutView<'a, T>, Trans<View<'b, T>>);
 add2!(MutView<'a, T>, View<'b, T>);
 
-/* FIXME(rust-lang/rust#20749)
-add0!(Trans<Mat<T>>, T);
-*/
+impl<'a, T> Add<T> for MutView<'a, T> where T: Axpy + Clone + One {
+    type Output = MutView<'a, T>;
+
+    fn add(mut self, rhs: T) -> MutView<'a, T> {
+        self.add_assign(rhs);
+        self
+    }
+}
+
+add1_!(f32, MutView<'a, f32>);
+add1_!(f64, MutView<'a, f64>);
+add1_!(Complex<f32>, MutView<'a, Complex<f32>>);
+add1_!(Complex<f64>, MutView<'a, Complex<f64>>);
+
 add1!(Trans<Mat<T>>, &'a Mat<T>);
 add2!(Trans<Mat<T>>, &'a MutView<'b, T>);
 add1c!(Trans<Mat<T>>, Scaled<T, View<'a, T>>);
@@ -182,9 +248,20 @@ add2!(Trans<Mat<T>>, &'a Trans<MutView<'b, T>>);
 add1!(Trans<Mat<T>>, Trans<View<'a, T>>);
 add1!(Trans<Mat<T>>, View<'a, T>);
 
-/* FIXME(rust-lang/rust#20749)
-add1c!(Trans<MutView<'a, T>>, T);
-*/
+impl<T> Add<T> for Trans<Mat<T>> where T: Axpy + One {
+    type Output = Trans<Mat<T>>;
+
+    fn add(mut self, rhs: T) -> Trans<Mat<T>> {
+        self.add_assign(rhs);
+        self
+    }
+}
+
+add0_!(f32, Trans<Mat<f32>>);
+add0_!(f64, Trans<Mat<f64>>);
+add0_!(Complex<f32>, Trans<Mat<Complex<f32>>>);
+add0_!(Complex<f64>, Trans<Mat<Complex<f64>>>);
+
 add2!(Trans<MutView<'a, T>>, &'b Mat<T>);
 add3!(Trans<MutView<'a, T>>, &'b MutView<'c, T>);
 add2c!(Trans<MutView<'a, T>>, Scaled<T, View<'b, T>>);
@@ -193,19 +270,55 @@ add3!(Trans<MutView<'a, T>>, &'b Trans<MutView<'b, T>>);
 add2!(Trans<MutView<'a, T>>, Trans<View<'b, T>>);
 add2!(Trans<MutView<'a, T>>, View<'b, T>);
 
+impl<'a, T> Add<T> for Trans<MutView<'a, T>> where T: Axpy + Clone + One {
+    type Output = Trans<MutView<'a, T>>;
+
+    fn add(mut self, rhs: T) -> Trans<MutView<'a, T>> {
+        self.add_assign(rhs);
+        self
+    }
+}
+
+add1_!(f32, Trans<MutView<'a, f32>>);
+add1_!(f64, Trans<MutView<'a, f64>>);
+add1_!(Complex<f32>, Trans<MutView<'a, Complex<f32>>>);
+add1_!(Complex<f64>, Trans<MutView<'a, Complex<f64>>>);
+
 // row
-/* FIXME(rust-lang/rust#20749)
-add0!(RowVec<T>, T);
-*/
 add2!(RowVec<T>, &'a MutRow<'b, T>);
 add1!(RowVec<T>, Row<'a, T>);
 add1!(RowVec<T>, &'a RowVec<T>);
 add1!(RowVec<T>, Scaled<T, Row<'a, T>>);
 
-/* FIXME(rust-lang/rust#20749)
-add1!(MutRow<'a, T>, T);
-*/
+impl<T> Add<T> for RowVec<T> where T: Axpy + One {
+    type Output = RowVec<T>;
+
+    fn add(mut self, rhs: T) -> RowVec<T> {
+        self.add_assign(rhs);
+        self
+    }
+}
+
+add0_!(f32, RowVec<f32>);
+add0_!(f64, RowVec<f64>);
+add0_!(Complex<f32>, RowVec<Complex<f32>>);
+add0_!(Complex<f64>, RowVec<Complex<f64>>);
+
 add3!(MutRow<'a, T>, &'a MutRow<'b, T>);
 add2!(MutRow<'a, T>, Row<'a, T>);
 add2!(MutRow<'a, T>, &'a RowVec<T>);
 add2c!(MutRow<'a, T>, Scaled<T, Row<'a, T>>);
+
+impl<'a, T> Add<T> for MutRow<'a, T> where T: Axpy + One {
+    type Output = MutRow<'a, T>;
+
+    fn add(mut self, rhs: T) -> MutRow<'a, T> {
+        self.add_assign(rhs);
+        self
+    }
+}
+
+add1_!(f32, MutRow<'a, f32>);
+add1_!(f64, MutRow<'a, f64>);
+add1_!(Complex<f32>, MutRow<'a, Complex<f32>>);
+add1_!(Complex<f64>, MutRow<'a, Complex<f64>>);
--- src/scaled.rs
+++ src/scaled.rs
@@ -1,7 +1,41 @@
+use complex::Complex;
 use std::ops::Mul;
 
 use {Col, ColVec, Cols, Mat, MutCol, MutRow, MutView, Row, RowVec, Rows, Scaled, Trans, View};
 use traits::{Matrix, MatrixCol, MatrixRow};
+
+macro_rules! mul1 {
+    ($lhs:ty, $rhs:ty) => {
+        impl<'a> Mul<$rhs> for $lhs {
+            type Output = Scaled<$lhs, $rhs>;
+
+            fn mul(self, rhs: $rhs) -> Scaled<$lhs, $rhs> {
+                rhs * self
+            }
+        }
+    };
+    ($lhs:ty, $rhs:ty, $output:ty) => {
+        impl<'a> Mul<$rhs> for $lhs {
+            type Output = Scaled<$lhs, $output>;
+
+            fn mul(self, rhs: $rhs) -> Scaled<$lhs, $output> {
+                rhs * self
+            }
+        }
+    };
+}
+
+macro_rules! mul2 {
+    ($lhs:ty, $rhs:ty, $output:ty) => {
+        impl<'a, 'b> Mul<$rhs> for $lhs {
+            type Output = Scaled<$lhs, $output>;
+
+            fn mul(self, rhs: $rhs) -> Scaled<$lhs, $output> {
+                rhs * self
+            }
+        }
+    };
+}
 
 impl<'a, T, M> Iterator for Scaled<T, Rows<'a, M>> where
     T: Clone,
@@ -50,15 +84,10 @@ impl<'a, T> Mul<T> for Col<'a, T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, T> Mul<Col<'a, T>> for T {
-    type Output = Scaled<T, Col<'a, T>>;
-
-    fn mul(self, rhs: Col<'a, T>) -> Scaled<T, Col<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul1!(f32, Col<'a, f32>);
+mul1!(f64, Col<'a, f64>);
+mul1!(Complex<f32>, Col<'a, Complex<f32>>);
+mul1!(Complex<f64>, Col<'a, Complex<f64>>);
 
 impl<'a, T> Mul<T> for &'a ColVec<T> {
     type Output = Scaled<T, Col<'a, T>>;
@@ -68,15 +97,10 @@ impl<'a, T> Mul<T> for &'a ColVec<T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, T> Mul<&'a ColVec<T>> for T {
-    type Output = Scaled<T, Col<'a, T>>;
-
-    fn mul(self, rhs: &'a ColVec<T>) -> Scaled<T, Col<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul1!(f32, &'a ColVec<f32>, Col<'a, f32>);
+mul1!(f64, &'a ColVec<f64>, Col<'a, f64>);
+mul1!(Complex<f32>, &'a ColVec<Complex<f32>>, Col<'a, Complex<f32>>);
+mul1!(Complex<f64>, &'a ColVec<Complex<f64>>, Col<'a, Complex<f64>>);
 
 impl<'a, 'b, T> Mul<T> for &'a MutCol<'b, T> {
     type Output = Scaled<T, Col<'a, T>>;
@@ -86,15 +110,10 @@ impl<'a, 'b, T> Mul<T> for &'a MutCol<'b, T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, 'b, T> Mul<&'a MutCol<'b, T>> for T {
-    type Output = Scaled<T, Col<'a, T>>;
-
-    fn mul(self, rhs: &'a MutCol<'b, T>) -> Scaled<T, Col<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul2!(f32, &'a MutCol<'b, f32>, Col<'a, f32>);
+mul2!(f64, &'a MutCol<'b, f64>, Col<'a, f64>);
+mul2!(Complex<f32>, &'a MutCol<'b, Complex<f32>>, Col<'a, Complex<f32>>);
+mul2!(Complex<f64>, &'a MutCol<'b, Complex<f64>>, Col<'a, Complex<f64>>);
 
 // mat
 impl<'a, T> Mul<T> for &'a Mat<T> {
@@ -105,15 +124,10 @@ impl<'a, T> Mul<T> for &'a Mat<T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, T> Mul<&'a Mat<T>> for T {
-    type Output = Scaled<T, View<'a, T>>;
-
-    fn mul(self, rhs: &'a Mat<T>) -> Scaled<T, View<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul1!(f32, &'a Mat<f32>, View<'a, f32>);
+mul1!(f64, &'a Mat<f64>, View<'a, f64>);
+mul1!(Complex<f32>, &'a Mat<Complex<f32>>, View<'a, Complex<f32>>);
+mul1!(Complex<f64>, &'a Mat<Complex<f64>>, View<'a, Complex<f64>>);
 
 impl<'a, 'b, T> Mul<T> for &'a MutView<'b, T> {
     type Output = Scaled<T, View<'a, T>>;
@@ -123,15 +137,10 @@ impl<'a, 'b, T> Mul<T> for &'a MutView<'b, T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, 'b, T> Mul<&'a MutView<'b, T>> for T {
-    type Output = Scaled<T, View<'a, T>>;
-
-    fn mul(self, rhs: &'a MutView<'b, T>) -> Scaled<T, View<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul2!(f32, &'a MutView<'b, f32>, View<'a, f32>);
+mul2!(f64, &'a MutView<'b, f64>, View<'a, f64>);
+mul2!(Complex<f32>, &'a MutView<'b, Complex<f32>>, View<'a, Complex<f32>>);
+mul2!(Complex<f64>, &'a MutView<'b, Complex<f64>>, View<'a, Complex<f64>>);
 
 impl<'a, T> Mul<T> for &'a Trans<Mat<T>> {
     type Output = Scaled<T, Trans<View<'a, T>>>;
@@ -141,15 +150,10 @@ impl<'a, T> Mul<T> for &'a Trans<Mat<T>> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, T> Mul<&'a Trans<Mat<T>>> for T {
-    type Output = Scaled<T, Trans<View<'a, T>>>;
-
-    fn mul(self, rhs: &'a Trans<Mat<T>>) -> Scaled<T, Trans<View<'a, T>>> {
-        rhs * self
-    }
-}
-*/
+mul1!(f32, &'a Trans<Mat<f32>>, Trans<View<'a, f32>>);
+mul1!(f64, &'a Trans<Mat<f64>>, Trans<View<'a, f64>>);
+mul1!(Complex<f32>, &'a Trans<Mat<Complex<f32>>>, Trans<View<'a, Complex<f32>>>);
+mul1!(Complex<f64>, &'a Trans<Mat<Complex<f64>>>, Trans<View<'a, Complex<f64>>>);
 
 impl<'a, 'b, T> Mul<T> for &'a Trans<MutView<'b, T>> {
     type Output = Scaled<T, Trans<View<'a, T>>>;
@@ -159,15 +163,10 @@ impl<'a, 'b, T> Mul<T> for &'a Trans<MutView<'b, T>> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, 'b, T> Mul<&'a Trans<MutView<'b, T>>> for T {
-    type Output = Scaled<T, Trans<View<'a, T>>>;
-
-    fn mul(self, rhs: &'a Trans<MutView<'b, T>>) -> Scaled<T, Trans<View<'a, T>>> {
-        rhs * self
-    }
-}
-*/
+mul2!(f32, &'a Trans<MutView<'b, f32>>, Trans<View<'a, f32>>);
+mul2!(f64, &'a Trans<MutView<'b, f64>>, Trans<View<'a, f64>>);
+mul2!(Complex<f32>, &'a Trans<MutView<'b, Complex<f32>>>, Trans<View<'a, Complex<f32>>>);
+mul2!(Complex<f64>, &'a Trans<MutView<'b, Complex<f64>>>, Trans<View<'a, Complex<f64>>>);
 
 impl<'a, T> Mul<T> for Trans<View<'a, T>> {
     type Output = Scaled<T, Trans<View<'a, T>>>;
@@ -177,15 +176,10 @@ impl<'a, T> Mul<T> for Trans<View<'a, T>> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, T> Mul<Trans<View<'a, T>>> for T {
-    type Output = Scaled<T, Trans<View<'a, T>>>;
-
-    fn mul(self, rhs: Trans<View<'a, T>>) -> Scaled<T, Trans<View<'a, T>>> {
-        rhs * self
-    }
-}
-*/
+mul1!(f32, Trans<View<'a, f32>>);
+mul1!(f64, Trans<View<'a, f64>>);
+mul1!(Complex<f32>, Trans<View<'a, Complex<f32>>>);
+mul1!(Complex<f64>, Trans<View<'a, Complex<f64>>>);
 
 impl<'a, T> Mul<T> for View<'a, T> {
     type Output = Scaled<T, View<'a, T>>;
@@ -195,15 +189,10 @@ impl<'a, T> Mul<T> for View<'a, T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, T> Mul<View<'a, T>> for T {
-    type Output = Scaled<T, View<'a, T>>;
-
-    fn mul(self, rhs: View<'a, T>) -> Scaled<T, View<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul1!(f32, View<'a, f32>);
+mul1!(f64, View<'a, f64>);
+mul1!(Complex<f32>, View<'a, Complex<f32>>);
+mul1!(Complex<f64>, View<'a, Complex<f64>>);
 
 // row
 impl<'a, T> Mul<T> for Row<'a, T> {
@@ -214,15 +203,10 @@ impl<'a, T> Mul<T> for Row<'a, T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, T> Mul<Row<'a, T>> for T {
-    type Output = Scaled<T, Row<'a, T>>;
-
-    fn mul(self, rhs: Row<'a, T>) -> Scaled<T, Row<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul1!(f32, Row<'a, f32>);
+mul1!(f64, Row<'a, f64>);
+mul1!(Complex<f32>, Row<'a, Complex<f32>>);
+mul1!(Complex<f64>, Row<'a, Complex<f64>>);
 
 impl<'a, T> Mul<T> for &'a RowVec<T> {
     type Output = Scaled<T, Row<'a, T>>;
@@ -232,15 +216,10 @@ impl<'a, T> Mul<T> for &'a RowVec<T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, T> Mul<&'a RowVec<T>> for T {
-    type Output = Scaled<T, Row<'a, T>>;
-
-    fn mul(self, rhs: &'a RowVec<T>) -> Scaled<T, Row<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul1!(f32, &'a RowVec<f32>, Row<'a, f32>);
+mul1!(f64, &'a RowVec<f64>, Row<'a, f64>);
+mul1!(Complex<f32>, &'a RowVec<Complex<f32>>, Row<'a, Complex<f32>>);
+mul1!(Complex<f64>, &'a RowVec<Complex<f64>>, Row<'a, Complex<f64>>);
 
 impl<'a, 'b, T> Mul<T> for &'a MutRow<'b, T> {
     type Output = Scaled<T, Row<'a, T>>;
@@ -250,15 +229,10 @@ impl<'a, 'b, T> Mul<T> for &'a MutRow<'b, T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<'a, 'b, T> Mul<&'a MutRow<'b, T>> for T {
-    type Output = Scaled<T, Row<'a, T>>;
-
-    fn mul(self, rhs: &'a MutRow<'b, T>) -> Scaled<T, Row<'a, T>> {
-        rhs * self
-    }
-}
-*/
+mul2!(f32, &'a MutRow<'b, f32>, Row<'a, f32>);
+mul2!(f64, &'a MutRow<'b, f64>, Row<'a, f64>);
+mul2!(Complex<f32>, &'a MutRow<'b, Complex<f32>>, Row<'a, Complex<f32>>);
+mul2!(Complex<f64>, &'a MutRow<'b, Complex<f64>>, Row<'a, Complex<f64>>);
 
 // scaled
 impl<T, M> Mul<T> for Scaled<T, M> where T: Mul<Output=T> {
@@ -269,12 +243,18 @@ impl<T, M> Mul<T> for Scaled<T, M> where T: Mul<Output=T> {
     }
 }
 
-/* FIXME(rust-lang/rust#20749)
-impl<T, M> Mul<Scaled<T, M>> for T where T: Mul<Output=T> {
-    type Output = Scaled<T, M>;
+macro_rules! mul {
+    ($($ty:ty),+) => {
+        $(
+            impl<M> Mul<Scaled<$ty, M>> for $ty {
+                type Output = Scaled<$ty, M>;
 
-    fn mul(self, rhs: Scaled<T, M>) -> Scaled<T, M> {
-        rhs * self
+                fn mul(self, rhs: Scaled<$ty, M>) -> Scaled<$ty, M> {
+                    rhs * self
+                }
+            }
+        )+
     }
 }
-*/
+
+mul!(f32, f64, Complex<f32>, Complex<f64>);
