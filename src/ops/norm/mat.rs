7ed63b9a3a1f28141cefaa1bffcd7514486072fd
Jorge Aparicio <japaricious@gmail.com>
2015-05-11 10:10:19 -0500

    feat: impl Norm for SubMat[Mut]

--- src/ops/norm/mat.rs
+++ src/ops/norm/mat.rs
@@ -4,8 +4,8 @@ use blas::Nrm2;
 use onezero::Zero;
 
 use ops::norm;
-use traits::Norm;
-use {Mat, Transposed};
+use traits::{Norm, Slice};
+use {Mat, SubMat, SubMatMut, Transposed};
 
 // NOTE Core
 impl<T, U> Norm for Mat<T> where T: Nrm2<Output=U>, U: Add<Output=U> + Zero {
@@ -16,11 +16,33 @@ impl<T, U> Norm for Mat<T> where T: Nrm2<Output=U>, U: Add<Output=U> + Zero {
     }
 }
 
+// NOTE Core
+impl<'a, T, U> Norm for SubMat<'a, T> where T: Nrm2<Output=U>, U: Add<Output=U> + Zero {
+    type Output = U;
+
+    fn norm(&self) -> U {
+        if let Some(slice) = self.as_slice() {
+            norm::slice(slice)
+        } else {
+            unimplemented!();
+        }
+    }
+}
+
 // NOTE Secondary
 impl<T, U> Norm for Transposed<Mat<T>> where T: Nrm2<Output=U>, U: Add<Output=U> + Zero {
     type Output = U;
 
     fn norm(&self) -> U {
         self.0.norm()
+    }
+}
+
+// NOTE Forward
+impl<'a, T, U> Norm for SubMatMut<'a, T> where T: Nrm2<Output=U>, U: Add<Output=U> + Zero {
+    type Output = U;
+
+    fn norm(&self) -> U {
+        self.slice(..).norm()
     }
 }
